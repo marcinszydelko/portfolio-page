c53fdcd3f20ee2d1af3d982316657a48d5d74b05
Marcin <marcinszydelko@yahoo.com>
2019-12-31 02:19:04 +0000

    add main description

---
+++ src/components/MainDescription.vue
@@ -0,0 +1,18 @@
+<template lang="">
+  <div id="description">
+    <h1>Marcin Szydelko</h1>
+    <h2>Software Developer</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: "MainDescription"
+}
+</script>
+
+<style lang="css" scoped>
+#description{
+  position: relative;
+  top: 70px;
+}
+</style>
