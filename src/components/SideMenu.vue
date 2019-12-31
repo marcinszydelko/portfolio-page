d993c786d6808c694defed1bd1588b649955450d
Marcin <marcinszydelko@yahoo.com>
2019-12-31 02:17:58 +0000

    add side menu

---
+++ src/components/SideMenu.vue
@@ -0,0 +1,58 @@
+<template lang="html">
+  <div id="menu">
+    <!-- <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+      <el-radio-button :label="false">expand</el-radio-button>
+      <el-radio-button :label="true">collapse</el-radio-button>
+    </el-radio-group> -->
+    <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+      <el-menu-item index="1">
+        <i class="el-icon-s-home"></i>
+        <span slot="title">Home</span>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">Projects</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <i class="el-icon-document"></i>
+        <span slot="title">About me</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-message"></i>
+        <span slot="title">Contact</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SideMenu',
+  data() {
+      return {
+        isCollapse: true
+      };
+    },
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+
+  #menu{
+    position: fixed;
+    top:50%;
+    height: 400px;
+    margin-top: -200px;
+  }
+</style>
