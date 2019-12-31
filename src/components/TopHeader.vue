6a550acacee1a0f597ec3e0f690b538111fe3758
Marcin <marcinszydelko@yahoo.com>
2019-12-31 02:16:09 +0000

    add header

---
+++ src/components/TopHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="header">
+    <h1 id="logo" >MS</h1>
+    <el-button>Contact</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopHeader'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#header{
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+#contact-button{
+
+}
+
+</style>
